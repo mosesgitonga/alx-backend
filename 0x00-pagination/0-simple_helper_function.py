0cb92ebb1baadec0455c1ea5a59600d3e703f842
mosesgitonga <infosec947@gmail.com>
2024-01-28 11:14:01 +0300

    fixed docstring

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ simple helper function
 def index_range(page: int, page_size: int) -> tuple:
     """
     return: start and end index in a tuple
-    """"
+    """
     start_idx = (page - 1) * page_size
     end_idx = start_idx + page_size
 
