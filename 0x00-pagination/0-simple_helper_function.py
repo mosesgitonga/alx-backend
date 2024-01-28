2ab1a498a2bb8ca80a12e4799256304c9a1cad24
mosesgitonga <infosec947@gmail.com>
2024-01-28 11:12:26 +0300

    simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+simple helper function
+"""
+
 
 def index_range(page: int, page_size: int) -> tuple:
+    """
+    return: start and end index in a tuple
+    """"
     start_idx = (page - 1) * page_size
     end_idx = start_idx + page_size
 
