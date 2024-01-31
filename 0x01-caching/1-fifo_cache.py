9f7439ca82d35f31c0045883d816580549b59962
mosesgitonga <infosec947@gmail.com>
2024-01-31 10:28:38 +0300

    adding docstrings

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,12 +9,17 @@ class FIFOCache(BaseCaching):
     """inheriting from BaseCaching
     """
     def put(self, key, item):
+        """
+        put key with item into the cache_data dictionary
+        """
         if key and item:
             self.cache_data[key] = item
         else:
             return
 
     def get(self, key):
+        """get the value of a key
+        """
         if key in self.cache_data:
             value = self.cache_data.get(key)
             return value
