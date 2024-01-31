37d79035c8954f23ca5f619518a2e47ba35155d7
mosesgitonga <infosec947@gmail.com>
2024-01-31 12:23:54 +0300

    last in first out

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+caching
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    last in first out
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        put key with value
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = next(reversed(self.cache_data))
+                del self.cache_data[last_key]
+                print('DISCARD: {}'.format(last_key))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get value
+        """
+        if key in self.cache_data:
+            value = self.cache_data.get(key)
+            return value
+        return None
