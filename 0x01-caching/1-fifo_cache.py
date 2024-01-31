33ef22f0868d8e42c63dad8c8220c6c098280f21
mosesgitonga <infosec947@gmail.com>
2024-01-31 11:47:36 +0300

    added docstring

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,19 +4,28 @@ simple cache
 """
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
+    """
+    first in first out
+    """
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
+        """
+        put key with value
+        """
         if key and item:
-            if len(self.cache_data) >=  BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first_key = next(iter(self.cache_data))
                 del self.cache_data[first_key]
                 print('DISCARD: {}'.format(first_key))
             self.cache_data[key] = item
 
-    def get(self, key, value):
+    def get(self, key):
+        """get value
+        """
         if key in self.cache_data:
             value = self.cache_data.get(key)
             return value
