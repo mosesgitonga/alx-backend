1a74850090dc232658ab281cfe99390e05bc2ea7
mosesgitonga <infosec947@gmail.com>
2024-02-06 15:56:50 +0300

    pycodestyle and made file exec

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,12 +20,14 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale() -> str:
     """
     set best language
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def welcome() -> str:
