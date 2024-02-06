86b547326deff6b140af3bef2aac2165d22d9848
mosesgitonga <infosec947@gmail.com>
2024-02-06 15:45:52 +0300

    pick best language for client

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,16 +20,19 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
-
+@babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config('LANGUAGES'))
+    """
+    set best language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/')
 def welcome() -> str:
     """
     welcome page route
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
