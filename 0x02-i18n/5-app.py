54ab0b8c2834f8cf0afe3f22c8965fc86f5ab97c
mosesgitonga <infosec947@gmail.com>
2024-02-06 18:32:58 +0300

    before request

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Basic babel setup
+"""
+from flask import Flask, render_template, request
+from flask-login import login_as
+from flask_babel import Babel
+
+
+class Config():
+    """
+    configuration for babel
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    set best language
+    """
+    locale_arg = request.args.get('locale')
+    if locale_arg and locale_arg in app.config['LANGUAGES']:
+        return locale_arg
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+def get_user() -> Union[Dict, None]:
+    """get user from users
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    else:
+        return None
+
+@app.before_request
+def before_request() -> None:
+    """
+    run this operation before each request
+    """
+    user = get_user()
+    g.user = user
+
+@app.route('/')
+def welcome() -> str:
+    """
+    welcome page route
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
