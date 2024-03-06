74e2dd027bc1095b8e3e5fb4c209cf5f92baf709
mosesgitonga <infosec947@gmail.com>
2024-03-06 07:39:42 +0300

    create a simple job

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,23 @@
+import { createClient  } from "redis";
+
+const client = createClient()
+
+client
+    .on('connect', () => {
+        console.log('Redis client connected to the server')
+    })
+    .on('error', (err) => {
+        console.log(`Redis client not connected to the server: ${err}`)
+    })
+
+client.subscribe('holberton school channel')
+
+client.on('message', (channel, message) => {
+    if (channel === 'holberton school channel') {
+        console.log(message)
+        if (message === 'KILL_SERVER') {
+            client.unsubscribe(channel)
+            client.quit
+        }
+    }
+})
